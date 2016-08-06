87ea37d04490444c960dbbadd023029f8412883e
Nextzy <ping.comsci@gmail.com>
2016-08-06 22:31:32 +0700

    rename MainActivity to CalculatorActivity and implement CalculatorContractor.view

---
+++ app/src/main/java/com/nextzy/mrsmile/mvp_example/calculator/CalculatorActivity.java
@@ -0,0 +1,36 @@
+package com.nextzy.mrsmile.mvp_example.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nextzy.mrsmile.mvp_example.R;
+
+public class CalculatorActivity extends AppCompatActivity implements CalculatorContractor.View {
+
+    private CalculatorContractor.Presenter presenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+
+        presenter.createPresenter(this);
+    }
+
+    @Override
+    public void setPresenter(CalculatorContractor.Presenter presenter) {
+        this.presenter = presenter;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        presenter.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        presenter.onStop();
+    }
+}
