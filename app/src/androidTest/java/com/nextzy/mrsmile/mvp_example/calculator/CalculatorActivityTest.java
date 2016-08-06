32ebcb83ea11821445aecdb0ea3d28dcdff0b634
Nextzy <ping.comsci@gmail.com>
2016-08-06 23:20:24 +0700

    update CalculatorActivityTest check isDisplay Button Zero to Nine

--- app/src/androidTest/java/com/nextzy/mrsmile/mvp_example/calculator/CalculatorActivityTest.java
+++ app/src/androidTest/java/com/nextzy/mrsmile/mvp_example/calculator/CalculatorActivityTest.java
@@ -1,8 +1,18 @@
 package com.nextzy.mrsmile.mvp_example.calculator;
 
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 
+import com.nextzy.mrsmile.mvp_example.R;
+
+import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * Created by nextzy on 8/6/2016 AD.
@@ -11,4 +21,60 @@ public class CalculatorActivityTest {
 
     @Rule
     ActivityTestRule<CalculatorActivity> activityTestRule;
+
+    @Before
+    public void setUp() {
+        activityTestRule = new ActivityTestRule<>(CalculatorActivity.class);
+    }
+
+    @Test
+    public void isVisibleButtonZero() {
+        onView(withId(R.id.btn_zero)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void isVisibleButtonOne() {
+        onView(withId(R.id.btn_one)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void isVisibleButtonTwo() {
+        onView(withId(R.id.btn_two)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void isVisibleButtonThere() {
+        onView(withId(R.id.btn_there)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void isVisibleButtonFour() {
+        onView(withId(R.id.btn_four)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void isVisibleButtonFive() {
+        onView(withId(R.id.btn_five)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void isVisibleButtonSix() {
+        onView(withId(R.id.btn_six)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void isVisibleButtonSeven() {
+        onView(withId(R.id.btn_seven)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void isVisibleButtonEight() {
+        onView(withId(R.id.btn_eight)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void isVisibleButtonNine() {
+        onView(withId(R.id.btn_nine)).check(matches(isDisplayed()));
+    }
+
 }
