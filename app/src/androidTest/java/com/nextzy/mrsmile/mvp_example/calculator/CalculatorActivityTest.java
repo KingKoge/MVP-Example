f5c8cecf12319795df1b75d961c537ac3f7e4892
Nextzy <ping.comsci@gmail.com>
2016-08-06 23:08:42 +0700

    add CalculaterActivityTest

---
+++ app/src/androidTest/java/com/nextzy/mrsmile/mvp_example/calculator/CalculatorActivityTest.java
@@ -0,0 +1,14 @@
+package com.nextzy.mrsmile.mvp_example.calculator;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+
+/**
+ * Created by nextzy on 8/6/2016 AD.
+ */
+public class CalculatorActivityTest {
+
+    @Rule
+    ActivityTestRule<CalculatorActivity> activityTestRule;
+}
